23a20a3ea25db8b2f3517b6ca57f47ed6c898969
Murodil <murodil@cybertekschool.com>
2025-08-02 18:48:45 -0400

    Jenkins demo project

--- src/test/java/cydeo/pages/PracticePage.java
+++ src/test/java/cydeo/pages/PracticePage.java
@@ -35,22 +35,12 @@ public class PracticePage {
     }
 
     public void login() {
-        /*
         String username = ConfigurationReader.getProperty("username");
         String password = ConfigurationReader.getProperty("password");
-         */
-
-        String username=System.getenv("USERNAME");
-        System.out.println("username = " + username);
-        String password=System.getenv("PASSWORD");
-        System.out.println("password = " + password);
 
         usernameEl.sendKeys(username);
         passwordEl.sendKeys(password);
         loginButton.click();
-
-
-
     }
 
 }
